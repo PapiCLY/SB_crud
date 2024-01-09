0c6e15ef2c316583db5a2589ff5633a697c79c9c
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-01-08 22:32:37 -0500

    creating user model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const UserSchema =  new mongoose.Schema({
+    googleId: {
+        type: String,
+        required: true
+    },
+    googleId: {
+        type: String,
+        required: true
+    },
+    googleId: {
+        type: String,
+        required: true
+    },
+    googleId: {
+        type: String,
+        required: true
+    },
+    googleId: {
+        type: String,
+        required: true
+    },
+})
