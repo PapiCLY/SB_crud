112aee4f29bdbfbb3618ef26ce2de11fcca9d115
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-01-08 23:19:05 -0500

    working on client ID error

--- config/passport.js
+++ config/passport.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose')
 const User = require('../models/User')
 
 module.exports = function(passport){
-    passport.use(new GoogleStrategy({
+    passport.use(new GoogleStrategy(
+        {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: '/auth/google/callback'
-    },
-    async (accessToken, refreshToken, profile, done)=> {
+        },
+        async (accessToken, refreshToken, profile, done)=> {
         console.log(profile)
-    }
+        }
     ))
 
     passport.serializeUser((user, done) => {
