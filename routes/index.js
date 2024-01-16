24cf0a32a8e0bf2d9390796b83bd19b89c572a7e
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-01-16 05:53:49 -0500

    created stories route and error pages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {ensureAuth, ensureGuest} = require('../middleware/auth')
+const Story = require('../models/Story')
 
 //login/landing page
 //route - GET/
@@ -17,10 +18,15 @@ router.get('/', ensureGuest, (req,res)=>{
 //route - GET/dashboard
 //ensureAuth - will route a user to an authenticated page if they have already been confirmed logged in and authenticated. 
 //user will not be required to login again
-router.get('/dashboard', ensureAuth,  (req,res)=>{
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+router.get('/dashboard', ensureAuth, async (req,res)=>{
+    try {
+        const stories = await Story.find({ user: req.user.id}).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+        })
+    } catch (err) {
+        console.error(err)
+    }
 })
 
 
