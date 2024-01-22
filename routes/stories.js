88fe4a5a34c5a1502c24d5bbc23d0b7a5469027f
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-01-22 05:59:28 -0500

    post story error

--- routes/stories.js
+++ routes/stories.js
@@ -10,6 +10,20 @@ router.get('/add', ensureAuth, (req,res)=>{
 })
 
 
+//process the add form for user stories
+//route - POST /stories
+router.get('/', ensureAuth, async (req,res)=>{
+    try {
+        req.body.user = req.user.id
+        await Story.create(req,body)
+        res.redirect('/dashboard')
+    } catch (error) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
+
 
 
 module.exports = router
