5eb0e28558cfddfd34dc3ded4d97a21fe31c5dda
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-01-30 05:25:30 -0500

    edit story route works

--- routes/stories.js
+++ routes/stories.js
@@ -46,15 +46,15 @@ router.get('/', ensureAuth, async(req,res)=>{
 //show edit page
 //route - /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async(req,res)=>{
-    const story = await Story.findOneAndDelete({
-        _id: req.params.id
+    const story = await Story.findOne({
+        _id: req.params.id,
     }).lean()
 
     if(!story){
         return res.render('error/404')
     }
 
-    if(story.user !== req.user.id){
+    if(story.user != req.user.id){
         res.redirect('/stories')
     } else{
         res.render('stories/edit', {
