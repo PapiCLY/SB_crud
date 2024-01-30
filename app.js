5a86bbc3d15247c445f5f75d302a2149fb6ee766
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-01-30 06:03:56 -0500

    story edit error

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan =  require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
@@ -24,6 +25,16 @@ const app = express()
 //body parser
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+//Mthod override
+app.use(methodOverride(function (req,res){
+    if(req.body && typeof req.body === 'object' && '_method' in req.body){
+        // look in urlencoded POST bodies and delect it
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
 
 //logging morgan when running on dev server
 if(process.env.NODE_ENV === 'development'){
