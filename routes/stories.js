3db8218b0331836a31207c52ebc8ac1c0fa417a0
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-01-31 05:37:03 -0500

    delete button working

--- routes/stories.js
+++ routes/stories.js
@@ -46,43 +46,68 @@ router.get('/', ensureAuth, async(req,res)=>{
 //show edit page
 //route - /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async(req,res)=>{
-    const story = await Story.findOne({
-        _id: req.params.id,
-    }).lean()
 
-    if(!story){
-        return res.render('error/404')
+    try {
+        const story = await Story.findOne({
+            _id: req.params.id,
+        }).lean()
+    
+        if(!story){
+            return res.render('error/404')
+        }
+    
+        if(story.user != req.user.id){
+            res.redirect('/stories')
+        } else{
+            res.render('stories/edit', {
+                story,
+            })
+        }
+    } catch (error) {
+            console.error(err)
+            return res.render('error/500')
     }
-
-    if(story.user != req.user.id){
-        res.redirect('/stories')
-    } else{
-        res.render('stories/edit', {
-            story,
-        })
-    }
+    
 })
 
 //Update Story
 //route - PUT /stories/:id
 router.put('/:id', ensureAuth, async(req,res)=>{
-    let story = await Story.findById(req.params.id).lean()
+    try {
+        let story = await Story.findById(req.params.id).lean()
 
-    if(!story){
-        return res.render('error/404')
-    }
+        if(!story){
+            return res.render('error/404')
+        }
+    
+        if(story.user != req.user.id){
+            res.redirect('/stories')
+        } else{
+            story = await Story.findOneAndUpdate({ _id: req.params.id}, req.body, {
+                new: true,
+                runValidators: true
+            })
+    
+            res.redirect('/dashboard')
+        }
+    } catch(err){
+        console.error(err)
+        return res.render('error/500')
+    }    
+})
 
-    if(story.user != req.user.id){
-        res.redirect('/stories')
-    } else{
-        story = await Story.findOneAndUpdate({ _id: req.params.id}, req.body, {
-            new: true,
-            runValidators: true
-        })
-
+//Delete Story
+//route - /stories/:add
+router.delete('/:id', ensureAuth, async(req,res)=>{
+    try {
+        await Story.findOneAndDelete({ _id: req.params.id})
         res.redirect('/dashboard')
+    } catch(err){
+        console.error(err)
+        return res.render('error/500')
     }
 })
+
 
 
 
